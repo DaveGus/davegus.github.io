5df87b18b8b61e2cee7a8d46ac4a445dc3bfff32
Dave Gus <alliancesales@gmail.com>
2018-11-06 11:39:18 -0600

    fixed intro image

---
+++ javascript/experiments.js
@@ -0,0 +1,65 @@
+/*const monthlyRent = 500;
+const monthsInAYear = 12;
+const yearlyRent = monthlyRent * monthsInAYear;
+const yearsInADecade = 10;
+
+const decadeRent = yearlyRent * yearsInADecade;
+console.log(decadeRent, yearlyRent);
+
+const myName = "Dave Gus";
+const myAge = 48;
+
+const sentence = `Hello I am ${myName}. I am ${myAge}`;
+
+console.log(sentence);
+
+const isTheSkyBlue = true;
+
+let greeting;
+
+if (isTheSkyBlue) {
+    greeting = "The sky is blue!";
+} else {
+    console.log("No, we are in the Eastern Block in the 80's");
+}
+
+console.log(isTheSkyBlue);
+console.log(greeting);
+
+/*const num = 1.001;
+ console.log(num + 1.2)
+ answer in console = 2.2009999999999996
+
+const myVariable = 10
+
+ if (2 + 2 === 4) {
+console.log("Horray! etc. ")
+ } else {
+     console.log("panic");
+ }
+
+const friendsAtParty = 1;
+
+if (friendsAtParty === 0) {
+    console.log("Cool. More for me.")
+} else if (friendsAtParty <= 4) {
+    console.log("Perect amount to play MK");
+} else {
+    console.log("Woo. Party on Garth!");
+}
+
+let friendsAtParty = 0;
+while (friendsAtParty < 10) {
+//    friendsAtParty = friendsAtParty + 1;
+    friendsAtParty +=3;
+    friendsAtParty++;
+    console.log(friendsAtParty)
+}
+console.log(friendsAtParty);*/
+
+let friendsAtParty = 0;
+for (let i = 0; i < 10; i++) {
+    friendsAtParty++;
+    console.log(i);
+}
+console.log(friendsAtParty);
